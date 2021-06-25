a766715b88e4601b4a960543805d40e8339fb2d1
Le Xuan Truong <lxtgl1998@gmail.com>
2021-06-25 18:43:00 +0700

    03

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import "./App.scss";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 import DefaultLayout from "./components/Layout/DefaultLayout";
+
+import Home from "./pages/Home";
 function App() {
   return (
     <div className="app">
-      <DefaultLayout></DefaultLayout>
+      <BrowserRouter>
+        <DefaultLayout>
+          <Switch exact path="/">
+            <Route>
+              <Home />
+            </Route>
+          </Switch>
+        </DefaultLayout>
+      </BrowserRouter>
     </div>
   );
 }
